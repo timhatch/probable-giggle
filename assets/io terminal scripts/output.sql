600be7232dd84affd14227fa2d1150416554b890
Tim Hatch <tim.hatch@o2.co.uk>
2015-03-06 14:45:38 +0000

    r00032 Add script to get Team Results

--- assets/io terminal scripts/output.sql
+++ assets/io terminal scripts/output.sql
@@ -17,3 +17,16 @@ WHERE
 #    OR category IS "fj"
 ORDER BY comp1 DESC, comp1 DESC
 LIMIT 0,50;
+
+#
+# Team Results
+#
+SELECT
+    team                        AS "Team",
+    sum(comp1)                  AS "Points",
+    group_concat(data.name)     AS "Members"
+FROM result1
+JOIN data USING (PerId)
+WHERE team IS NOT null
+GROUP BY team
+ORDER BY Points DESC;
