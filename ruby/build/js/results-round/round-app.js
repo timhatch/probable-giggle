59a75cc67e8eab5c639729962e4a63b5ebd69dfe
Tim Hatch <tim.hatch@o2.co.uk>
2016-01-16 11:32:48 +0000

    r087 Move Nexus specific source files
    and duplicate these to create a new "Desktop" results page

---
+++ ruby/build/js/results-round/round-app.js
@@ -0,0 +1,48 @@
+//	-----------------------------------------------------
+//	CODEKIT DECLARATIONS
+//	-----------------------------------------------------
+/* global m                       */
+/* global mx                       */
+
+// @codekit-prepend "./nexus-settings.js"
+// @codekit-prepend "./nexus-header.js"
+// @codekit-prepend "./nexus-results.js"
+// @codekit-prepend "./nexus-person.js"
+// @codekit-prepend "./nexus-vm.js"
+
+window.App = window.App || {}
+
+// Store the connection status (in-memory)
+App.connectionStatus = m.prop(true)
+// Use session storage to contain view model parameters
+App.sessionStorage   = mx.storage( 'session' , mx.SESSION_STORAGE )
+
+App.SuperVC = {
+  controller: function(){
+    var defaults = {
+      WetId : null, Route : null, GrpId : null,
+      BlcNr : null,
+
+      State : false, WetNm : false
+    }
+    
+    // Symchronise memory and session storage on-load
+    this.ss     = App.sessionStorage.get('AppState')
+    if (!this.ss) { 
+      this.ss = defaults
+      App.sessionStorage.set('AppState', defaults) 
+    } 
+
+    this.person = App.Person
+    this.vm     = App.VM(this.person, this.ss)
+  },
+  
+  view: function(ctrl){
+    var vm = ctrl.vm
+    return [
+      m.component(App.headerVC, vm),
+      (!!vm.ss.State) ? m.component(App.ResultsVC, vm) : m.component(App.settingsVC, vm)
+    ]
+  }
+}
+m.mount(document.body, App.SuperVC)
---
+++ ruby/build/js/results-round/round-settings.js
@@ -0,0 +1,84 @@
+/***********************************
+* CODEKIT DECLARATIONS
+***********************************/
+/* global m                       */
+/* global App                     */
+
+window.App = window.App || {}
+
+App.settingsVC = {
+  // Chnage this to vm, as the sessiondata is callable from the vm...
+  controller: function(vm){
+    this.ss = vm.ss
+    
+    this.fetch = function(){
+      // Break if a required value has not been provided...
+      // Note that we're not validating date here...
+      for (var prop in vm.ss) { if (vm.ss[prop] === null) return }
+      
+      // If all values have been provided, then fetch the competition ID from the server
+      m.request({ 
+        method: 'GET', 
+        url   : '/competition',
+        data  : { wet_id: vm.ss.WetId }
+      })
+      .then(function(resp){
+        try {
+          var title = resp.title || '-'
+          title +=  ' / '+vm.ss.Route+' / '+vm.ss.GrpId+' / '+vm.ss.BlcNr 
+          
+          vm.ss.WetNm = title
+          vm.ss.State = true
+          App.sessionStorage.set('AppState', vm.ss)
+        }
+        catch (err) {
+          window.console.log('invalid response : '+err) 
+        }
+        // TODO Need to clear or reset the model & view model when changing settings
+        vm.reset()
+        App.connectionStatus(true)
+      })
+      .then(null, function(){
+        App.connectionStatus(false)
+      })
+    }
+  },
+  
+  view: function(ctrl){
+    var ss = ctrl.ss
+    return m("div#settings",[
+      m.component(App.ParamSV, ss, { key: 'WetId', text: "competition", pattern: "[0-9]" }),
+      m.component(App.ParamSV, ss, { key: 'Route', text: "round" }),
+      m.component(App.ParamSV, ss, { key: 'GrpId', text: "category" }),
+      m.component(App.ParamSV, ss, { key: 'BlcNr', text: "boulder", pattern: "[0-9]" }),
+      m("button.save", { 
+        type    : "primary", 
+        outline : true, 
+        upper   : true,
+        onclick : ctrl.fetch.bind(ctrl)
+      }, "save")
+    ])
+  }
+}
+
+App.ParamSV = {
+  controller: function(sessiondata, params){
+    this.ss     = sessiondata
+    this.params = params
+    // Note that this stores all keys as strings...
+    this.set = function(val){
+      sessiondata[params.key] = val.toUpperCase() || null
+    }
+  },
+  
+  view: function(ctrl){
+    return m("div.modal", [
+      m("label", ctrl.params.text),
+      m("input[type=text]", {
+        onchange: m.withAttr("value", ctrl.set.bind(ctrl)),
+        pattern : ctrl.params.pattern || null,
+        value   : ctrl.ss[ctrl.params.key]
+      })
+    ])
+  }
+}
