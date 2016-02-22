c9204475e5cf2d89b0af9b5e31455cfc24f946a4
Tim Hatch <tim.hatch@o2.co.uk>
2016-02-22 19:39:41 +0000

    r149 Save results when moving to the next climber
    Moving to the next climber triggers a model save if:
    a) the climber has not achieved a bonus
    b) the climber has got a bonus but not a top
    c) the climber has had their result reset to zero

--- ruby/build/js/personselector_viewcontroller.js
+++ ruby/build/js/personselector_viewcontroller.js
@@ -10,6 +10,24 @@ App.PersonSelectorView = {
             
     this.incrementStarter = function(){
       var val = vm.start_order + 1
+      // No top
+      if (vm.result.b > 0 && vm.result.t === null) { 
+        vm.result.t = 0 
+        vm.save()
+      }
+      // No bonus
+      else if (vm.result.a > 0 && vm.result.b === null) { 
+        vm.result.b = 0 
+        vm.save()      
+      }
+      // Result manually zeroed
+      else if (vm.result === 0) { 
+        vm.result.a = vm.result.b = vm.result.t = null 
+        vm.save()
+      }
+      
+      // Fetch the next set of data 
+      // TODO Add error handling to deal with running past the last climber...
       vm.fetch(val)
     }
   },
