f97bb43ba7ae5e92a9b9006463ce1085ba9af653
Tim Hatch <tim.hatch@o2.co.uk>
2016-03-07 13:15:23 +0000

    r166 Update the startlist creation controller (WIP)

--- ruby/controllers/startlist_controller.rb
+++ ruby/controllers/startlist_controller.rb
@@ -1,66 +1,83 @@
 # Handlers for '/startlist' routes 
 #
 
+require 'csv'
+
 module Perseus
   class StartlistController < ApplicationController
   
-    # Delete one or more competitors
-    #
-    def delete_competitors params
-      # Guard against (accidental) inputs that would result in bulk deletions...
-      # i.e. if no comp/route/group is specified
-      params[:wet_id] = params[:wet_id] || 0
-      params[:grp_id] = params[:grp_id] || 0
-      params[:route]  = params[:route]  || 4
-      
-      DB[:Results]
-        .where(params)
-        .delete
-    end
-
-    def insert_quali_list params
-      delete_competitors(params)
-      # TODO
-      # Add logic...
-      # Here we probably need to put an automatic allocation of group depending upon the 
-      # climber's gender and age... 
-    end
+    defaults = { wet_id: 0, route: 0, grp_id: 0, quota: 1 }
     
-    # Required params are wet_id, grp_id, route, next_route, quota
-    #
-    def insert_final_list params
-      next_route = params.delete(:next_route)
-      quota      = params.delete(:quota)
+    # HELPERS
+    helpers Perseus::CWIFResultsModus
+    helpers Perseus::IFSCResultsModus
 
-      # Query the database to get the list of qualified climbers
-      dataset = DB[:Ranking]
-        .select(:per_id, :result_rank)
-        .where(params)
-        .where(result_rank: 1..quota)
-        .reverse_order(:result_rank)
-        .all
-      
-      # Delete any pre-existing startlist
-      params[:route] = next_route
-      delete_competitors(params)
-      
-      # Create a new startlist from an ordered array
+    # Move to the next round, delete any existing startlist and [re-]create
+    def create_startlist params, dataset
+      DB[:Results].where(params).delete
       dataset.each.with_index(1) do |person, i|
         person.merge!(params)
-        person[:start_order]    = i
-        person[:rank_prev_heat] = person.delete(:result_rank)
+        person[:start_order] ||= i
         DB[:Results].insert(person)
-      end
+      end      
+    end      
+    
+    # 
+    # @params: wet_id, grp_id, route, quota
+    #
+    def create_from_cwif_results params
+      quota = params.delete(:quota)
+      
+      # Fetch the results of the previous round
+      dataset = DB[:Results]
+      .where(params)
+      .select(:per_id)
+      .select_append{
+        rank.function.over(order: CWIFResultsModus.rank_generator)
+        .as(:rank_prev_heat)}
+      .reverse_order(:rank_prev_heat)
+      .all
+      
+      # Apply the quota and call the create_startlist method 
+      dataset.keep_if { |row| row[:rank_prev_heat] <= quota }
+      params[:route] += 1
+      create_startlist(params, dataset)
     end
-  
-    # Route handling
-    #    
-    post '/' do
-      hash = Hash[params.map{ |(k,v)| [k.to_sym,v.to_i] }]
-      insert_final_list hash
+    
+    def create_from_ifsc_results params
+      quota = params.delete(:quota)
+      
+      # Fetch the results of the previous round
+      dataset = DB[:Results]
+      .where(params)
+      .select(:per_id)
+      .select_append{
+        rank.function.over(order: IFSCResultsModus.rank_generator)
+        .as(:rank_prev_heat)}
+      .reverse_order(:rank_prev_heat)
+      .all
+      
+      # Apply the quota and call the create_startlist method 
+      dataset.keep_if { |row| row[:rank_prev_heat] <= quota }
+      params[:route] += 1
+      create_startlist(params, dataset)
+    end
+    
+    post '/import' do
+    
+    end
+    
+    post '/cwif' do
+      args = defaults.merge Hash[params.map{ |(k,v)| [k.to_sym,v.to_i] }]
+      create_from_cwif_results(args)
       200
     end
-  
+    
+    post  '/ifsc' do
+      args = defaults.merge Hash[params.map{ |(k,v)| [k.to_sym,v.to_i] }]
+      create_from_ifsc_results(args)
+      200
+    end
+      
   end
 end
-
