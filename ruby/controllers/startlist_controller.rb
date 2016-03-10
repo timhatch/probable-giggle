b0c6c976f7eadd965c7bcfe45851563d1ef87e06
Tim Hatch <tim.hatch@o2.co.uk>
2016-03-10 20:49:50 +0000

    r173 Refactor StartlistController
    - Add the ability to import a startlist based on a csv file containing start_order / per_id pairings
    - Refactor the create_from_cwif_results method
    - Refactor the create_from_ifsc_results method

--- ruby/controllers/startlist_controller.rb
+++ ruby/controllers/startlist_controller.rb
@@ -6,13 +6,14 @@ require 'csv'
 module Perseus
   class StartlistController < ApplicationController
   
-    defaults = { wet_id: 0, route: 0, grp_id: 0, quota: 1 }
+    defaults = { wet_id: 0, route: 0, grp_id: 0 }
     
     # HELPERS
     helpers Perseus::CWIFResultsModus
     helpers Perseus::IFSCResultsModus
 
     # Move to the next round, delete any existing startlist and [re-]create
+    # Not the 
     def create_startlist params, dataset
       DB[:Results].where(params).delete
       dataset.each.with_index(1) do |person, i|
@@ -22,11 +23,12 @@ module Perseus
       end      
     end      
     
-    # 
+    # Create a startlist from results for a previous round where the ranking for that round
+    # was calculated using the CWIF 10/7/4/b modus  
     # @params: wet_id, grp_id, route, quota
     #
     def create_from_cwif_results params
-      quota = params.delete(:quota)
+      quota = params[:quota] || 1
       
       # Fetch the results of the previous round
       dataset = DB[:Results]
@@ -44,8 +46,12 @@ module Perseus
       create_startlist(params, dataset)
     end
     
+    # Create a startlist from results for a previous round where the ranking for that round
+    # was calculated using the IFSC T/TA/B/BA modus  
+    # @params: wet_id, grp_id, route, quota
+    #
     def create_from_ifsc_results params
-      quota = params.delete(:quota)
+      quota = params[:quota] || 1
       
       # Fetch the results of the previous round
       dataset = DB[:Results]
@@ -63,8 +69,34 @@ module Perseus
       create_startlist(params, dataset)
     end
     
-    post '/import' do
+    # Parse a csv file containing (as a minimum) per_id and start_order pairs
+    # creating (for each line) a hash corresponding to the data and return an 
+    # array of these hashes
+    #
+    def parse_csv_file params
+      array = []
+      if params[:file]
+        file = params[:file][:tempfile]
+        
+        CSV.foreach(file, { headers: true, converters: :numeric }) do |row|
+          array << Hash[row.to_hash.map{|(k,v)| [k.to_sym,v]}]
+        end
+      end
+      array
+    end
     
+    # Create a startlist from a CSV formatted file
+    # Assume that the CSV file contains the following data:
+    # per_id, start_order
+    # Thie method assumes that wet_id, grp_id and route parameters are also provided
+    # which are merged into the default object and passed to the create_startlist method
+    # 
+    post '/import' do
+      list = parse_csv_file({ file: params.delete("file") })
+      
+      args = defaults.merge Hash[params.map{ |(k,v)| [k.to_sym,v] }]
+      create_startlist(args, list)
+      200
     end
     
     post '/cwif' do
