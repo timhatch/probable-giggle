781d52e70715015145ace97b630ba56ee32442c5
Tim Hatch <tim.hatch@o2.co.uk>
2016-04-15 14:42:56 +0100

    Adding comments for clarity

--- ruby/scratch files/test_stats.rb
+++ ruby/scratch files/test_stats.rb
@@ -10,12 +10,16 @@ require 'csv'
 # address format for a non-passwork protected BD "postgres://[user]@localhost:5432/[name]"
 # address format for a passwork protected BD "postgres://[user][:password]@localhost:5432/[name]"
 #
+
+#ENV['DATABASE_URL'] = "postgres://postgres@localhost:5432/test"
+#ENV['DATABASE_URL'] = "postgres://postgres@melody.local:5432/test"
+
 DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://timhatch@localhost:5432/test")
 DB.extension :pg_array      # Needed to insert arrays
 Sequel.extension :pg_array_ops  # Needed to query stored arrays
 
 
-params = {wet_id: 1584, route: 3, grp_id: 5 }
+params = {wet_id: 2, route: 1, grp_id: 5 }
 
 # USE A FILTER TO SCREEN OUT JUNIOES (CWIF)
 
@@ -27,7 +31,6 @@ juniors = DB[:Climbers].where{birthyear > year - 19}
 
 # TEST IFSC SORTING AND CWIF QUALIFICATION SORTING
 #
-
 module IFSCResultsModus
   module_function
   def result_generator
@@ -48,37 +51,58 @@ module IFSCResultsModus
 end
 
 module CWIFResultsModus
-  module_function
-  def result_generator
-    t  = Sequel.pg_array_op(:sort_values)[1] * 13
-    ta = Sequel.pg_array_op(:sort_values)[2] * 3
-    b  = Sequel.pg_array_op(:sort_values)[3].cast(:text) 
+module_function
+#  def result_generator
+#    t  = Sequel.pg_array_op(:sort_values)[1] * 13
+#    ta = Sequel.pg_array_op(:sort_values)[2] * 3
+#    b  = Sequel.pg_array_op(:sort_values)[3].cast(:text) 
+#    
+#    Sequel.cast(t - ta, :text) + "b" + b 
+#  end
+def rank_generator
+  t  = Sequel.pg_array_op(:sort_values)[1] * 13
+  ta = Sequel.pg_array_op(:sort_values)[2] * 3
+  b  = Sequel.pg_array_op(:sort_values)[3]
+  
+  [(t - ta).desc, b.desc]
+end    
+end
+# NOTE
+# Test passing in a reference to a ranking method, to replace
+# StartlistController::create_from_cwif_results() and
+# StartlistController::create_from_ifsc_results()
+# by a single function
+class TestController  
+  def get_results params, rank_calculator
+    who = :Climbers
     
-    Sequel.cast(t - ta, :text) + "b" + b 
+    DB[:Results]
+      .where(params)
+      .select(:wet_id, :grp_id, :route)
+      .select_append(:result_json, :rank_prev_heat, :start_order, :sort_values)
+      .select_append{rank.function.over(order: rank_calculator).as(:result_rank)}
+      .join(who, :per_id => :per_id)
+      .order(:start_order)
+      .limit(10)
+      .all
   end
-  def rank_generator
-    t  = Sequel.pg_array_op(:sort_values)[1] * 13
-    ta = Sequel.pg_array_op(:sort_values)[2] * 3
-    b  = Sequel.pg_array_op(:sort_values)[3]
-    
-    [(t - ta).desc, b.desc]
-  end    
 end
 
-who = :Climbers
-#who = juniors
+hash_reference = { :method => :CWIFResultsModus }
+mod    = hash_reference[:method]
+p mod.class
+mod    = CWIFResultsModus 
+method = mod.rank_generator
 
-data = DB[:Results]
-  .where(params)
-  .select(:wet_id, :grp_id, :route)
-  .select_append(:Results__per_id, :lastname, :firstname, :nation, :birthyear)
-  .select_append(:result_json, :rank_prev_heat, :start_order, :sort_values)
-  .select_append{
-    rank.function.over(
-    partition: [:wet_id, :grp_id, :route],
-    order: IFSCResultsModus.rank_generator)
-    .as(:result_rank)}
-  .select_append(Sequel.as(IFSCResultsModus.result_generator, :result))
-  .join(who, :per_id => :per_id)
-  .order(:start_order)
-  .all
+instance = TestController.new
+p instance.get_results params, method
+
+
+#  .select_append(:Results__per_id, :lastname, :firstname, :nation, :birthyear)
+
+#row[:sort_values]
+#RB = Sequel.connect("postgres://postgres@melody.local:5432/postgres@melody.local/test")
+#RB = Sequel.connect("postgres://postgres@melody.local:5432/test")
+
+#p RB.drop_table(:Params)
+
