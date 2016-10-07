6eb188bf5e46d84291509b118341e2f2fddb20f0
Tim Hatch <tim.hatch@o2.co.uk>
2016-10-07 14:55:51 +0100

    Use the Sinatra SSE Gem for streaming.
    Probably we will end up replacing this, using the NGINX nchan extension in its place.

--- ruby/controllers/event_controller.rb
+++ ruby/controllers/event_controller.rb
@@ -9,60 +9,26 @@ require 'sinatra/sse'
 # 
 module Perseus
   class EventController < Perseus::ApplicationController
-    # Use the Server-Sent Event gem.
-    # Difficulty being its not obvious how to broadcast events from an internal trigger
     include Sinatra::SSE
 
-    connections   = []
-    notifications = []
-
-    def timestamp
-      Time.now.strftime("%H:%M:%S")
-    end
- 
-    get '/', provides: 'text/event-stream' do
-      stream :keep_open do |out|
-        connections << out
-        # Send a keep-alive message to stop the link timing out
-        # This is not strictly needed for a browser connection as most/all browsers will
-        # reopen the connection when a new message is sent
-        EM.add_periodic_timer(28) { out << "data: \n\n" }
-
-        #out.callback on stream close evt. 
-        out.callback { connections.delete(out) }
+    connections = []
+    
+    # Handle "receiver" connections
+    get '/' do
+      sse_stream do |out|
+        connections.push out
       end
-    end
-
-    # Simple post responder - here we just echo the received message to all active streaming connections
-    post '/' do
-      # Add s timestamp to the notification
-      notification = params.merge( { 'connections' => connections.count, 'timestamp' => timestamp }).to_json
-      notifications << notification
-      # Retain only the last 10 notifications
-      notifications.shift if notifications.length > 10
-      
-      connections.each { |out| out << "data: #{notification}\n\n"}
-      puts params
     end
     
-    active_connections = []
-
-    get '/gem' do
-      sse_stream do |out|
-        active_connections.push out
-
-        # EM.add_periodic_timer(1) do 
-        #   out.push :event => 'timer', :data => Time.now.to_s
-        # end
-      end
+    # Handle "transmitter" connections
+    post '/' do
+      # TODO: Need to handle all data processing here, updates to POSTGRES etc.
+      results = params.merge(timestamp: Time.now.to_s).to_json
+      connections.each { |out| out.push data: results }
+      puts 204 
     end
-
-    post '/gem' do
-      active_connections.each { |out| out.push :event => 'message', :data => params['message'] + ' ' + Time.now.to_s}
-      puts params
-    end
-
    
+    # Handle the entry page - note that this is not a streaming connection
     get '/display' do
       haml :events
     end
