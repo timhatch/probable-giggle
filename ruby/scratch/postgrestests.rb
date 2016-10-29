4e4ae293d1a1d191a1e8e3eed26539c6d51d11b1
Tim Hatch <tim.hatch@o2.co.uk>
2016-10-29 09:56:39 +0100

    Comment postgrestests.rb
    Add tests/examples for jsonb use in postgres,
    also a means of migrating existing string based json stores

--- ruby/scratch/postgrestests.rb
+++ ruby/scratch/postgrestests.rb
@@ -2,20 +2,6 @@ require 'sequel'
 require 'pg'
 require 'json'
 
-# Interrogate a hash 
-def get_attempts type, result, arr
-  regex   = Regexp.new "#{type}([0-9]{1,})"
-  matched = regex.match(result)
-  if matched
-    arr[0] += 1
-    arr[1] += matched.captures.to_a.first.to_i
-  end
-end
-
-#res = "a2b1t2"
-#arr = [0,0]
-#p get_attempts("t", res, arr)  
-
 # Use a lambda instead of a function
 def gen_attempts type
   regex    = Regexp.new "#{type}([0-9]{1,})"
@@ -28,64 +14,41 @@ end
 tops = gen_attempts "t"
 #p tops["a3b1t3"]
 
-def set_overall result_json
-  tarr = [0,0]
-  barr = [0,0]
-  result_json.each do |key,value|
-    get_attempts "t", value, tarr
-    get_attempts "b", value, bar
-  end
-  tarr + barr
-end
 
-#obj = { "p2" => "a3b2", "p1" => "a4b2t4" }
-#resArray = set_overall obj
-
+# TEST PG_ARRAY AND PG_JSON EXTENSIONS FOR SEQUEL 
+# 
+#
 DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://timhatch@localhost:5432/test")
 DB.extension :pg_array, :pg_json
-Sequel.extension :pg_array_ops  # Needed to query stored arrays?
+Sequel.extension :pg_array_ops
 Sequel.extension :pg_json
 
+# ruby "nil" is automatically translated into JSON "null"
+testhash = { title: { a: 1, b: nil, t: nil } }
 
-#dataset = DB[:Results]
-#  .where({wet_id: 99, per_id: 1030, route: 1})
+dataset = DB[:books]
+  .where({id: 1})
+  .update({
+    datab: Sequel.pg_jsonb(testhash)
+  })
 
-#p JSON.parse dataset.first[:result_json]
+# TEST JOINING TWO TABLES VIA A COMMON PARAMETER
+# Get some test data
+dataset = DB[:Results].join(:Climbers, [:per_id]).where({per_id: 1030})
+p dataset.first
 
-#testhash = { title: { a: 1, b: 1, t: 1 } }
-#p testhash
-#testjson = testhash.to_json
-#p testjson
-
-#dataset = DB[:books]
-#  .where({id: 1})
-#  .update({
-#    datab: Sequel.pg_jsonb(testhash)
-#  })
-
+# COPY A JSON OBJECT STORED AS A STRING ACROSS INTO A JSONB COLUMN
 # Copy all strongified results from result_json into result_jsonb as actual json objects
-dataset = DB[:Results]#.where({per_id: 1030})
-
-dataset.each do |item|
-  id   = item[:per_id]
-  hash = JSON.parse item[:result_json]
-
-  dataset.where({ per_id: id }).update({ result_jsonb: Sequel.pg_jsonb(hash) })
-end
-
-
-
-#dataset.update(result: "4t4 4b4")
-#pg_arr = Sequel.pg_array(resArray)
-#dataset.update(sort_values: Sequel.pg_array(resArray))
-
-# def update_result arr
-#   arr[0].to_s << 't' << arr[1].to_s << ' ' << arr[2].to_s << 'b' << arr[3].to_s
+# dataset = DB[:Results]
+# dataset.each do |item|
+#   id   = item[:per_id]
+#   hash = JSON.parse item[:result_json]
+# 
+#   dataset.where({ per_id: id }).update({ result_jsonb: Sequel.pg_jsonb(hash) })
 # end
 
-#arr = [1,2,3,3]
-#p update_result arr
-#def gen_times factor
+
+
 
   #THREE METHODS FOR ORDERING IN SEQUEL
 #  p DB[:Forecast]
