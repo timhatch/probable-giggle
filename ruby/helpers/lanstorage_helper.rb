7bba0913c148094d5a66433a0d4bf6051e6a2ed4
Tim Hatch <tim.hatch@o2.co.uk>
2017-01-01 10:07:01 +0000

    Modularise the Competitor store accessors and add discrete tests

--- ruby/helpers/lanstorage_helper.rb
+++ ruby/helpers/lanstorage_helper.rb
@@ -199,34 +199,42 @@ end
 #
 module Perseus
   module LANStorageAPI
-    module_function
+    module Competitors
+      # Notionally private methods
+      private_class_method
 
-    # Helper method to import competitors into the local database
-    # The "replace" operator is not supported for postgres databases, so use a workaround
-    # We assume that the compatitors parameter is an array of hash objects, each object
-    # containing the following parameters:
-    # per_id, lastname, firstname, federation, nation, birthyear
-    # NOTE: The has conetne is assumed to be string based (not symbol based)
-    #
-    def insert_registrants competitors
-      competitors.each do |person|
+      def self.query person
         person['per_id'] = person['PerId'] unless person['PerId'].nil?
-        record = DB[:Climbers].where(per_id: person['per_id'].to_i)
-        unless record.first
-          record.insert(
-            per_id:    person['per_id'].to_i,
-            lastname:  person['lastname'],
-            firstname: person['firstname'],
-            club:      person['federation'],
-            nation:    person['nation'],
-            birthyear: person['birthyear'].to_i
-          )
+        DB[:Climbers].where(per_id: person['per_id'].to_i)
+      end
+
+      module_function
+      
+      # Helper method to import competitors into the local database
+      # The "replace" operator is not supported for postgres databases, so use a workaround
+      # We assume that the compatitors parameter is an array of hash objects, each object
+      # containing the following parameters:
+      # per_id, lastname, firstname, federation, nation, birthyear
+      # NOTE: The hash contents are assumed to be string (ot symbol) based. This works for the
+      # eGroupware response
+      def insert competitors
+        competitors.each do |person|
+          record = query(person)
+          unless record.first
+            record.insert(
+              per_id:    person['per_id'].to_i,
+              lastname:  person['lastname'],
+              firstname: person['firstname'],
+              club:      person['federation'],
+              nation:    person['nation'],
+              birthyear: person['birthyear'].to_i
+            )
+          end
         end
       end
     end
   end
 end
-
 
 module Perseus
   module IFSCBoulderModus
--- ruby/test/test_lanstorage_helper.rb
+++ ruby/test/test_lanstorage_helper.rb
@@ -38,3 +38,21 @@ class Session < Test::Unit::TestCase
     assert_equal(resp6, Session.data)
   end
 end
+
+class Competitors < Test::Unit::TestCase
+  include Perseus::LANStorageAPI
+
+  def test_query
+    test_result = DB[:Climbers].where(per_id: 1030).first
+    assert_equal(test_result, Competitors.query('PerId' => 1030).first)
+  end
+
+  def test_insert
+    test_array  = [{ 'per_id' => 1_000_000, 'lastname' => 'test' }]
+    Competitors.insert(test_array)
+    check_result = DB[:Climbers].where(per_id: 1_000_000).first
+    check_result.keep_if { |_k, v| !v.nil? }
+    test_result = { per_id: 1_000_000, lastname: 'test', birthyear: 0 }
+    assert_equal(test_result, check_result)
+  end
+end
