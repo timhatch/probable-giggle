9e7938b364d7d9610955face3e97b84743351196
Tim Hatch <tim.hatch@o2.co.uk>
2017-01-01 19:25:41 +0000

    Run egroupware_helper.rb past rubocop

--- ruby/helpers/egroupware_helper.rb
+++ ruby/helpers/egroupware_helper.rb
@@ -12,8 +12,7 @@ require 'date'
 module Perseus
   # EGroupwarePublicAPI - Helper functions to access the eGroupware Public API
   module EGroupwarePublicAPI
-    # private_class_method
-    module_function
+    private_class_method
 
     # API Accessor
     # Interrogate EGroupware using the JSON API, Aborting if the site cannot be accessed or the
@@ -21,7 +20,7 @@ module Perseus
     # Returns a JSON object (with keys represented as strings not symbols)
     # @params - Hash object containing variables (see the EGroupware API reference for options
     #
-    def _get_json params
+    def self.get_json params
       url = 'http://egw.ifsc-climbing.org/egw/ranking/json.php'
       begin
         response = HTTParty.get(url, query: params)
@@ -32,19 +31,18 @@ module Perseus
       end
     end
 
-    # module_function
+    module_function
 
     # Fetch the list of competitors/team officials registered for the competition
     # Use a default value for grpid (to return the full dataset if no category is supplied)
     def get_starters params
       grpid = params[:grp_id].to_i || 0
-      data  = _get_json(comp: params[:wet_id].to_i || 0, type: 'starters')
-      unless data.nil? 
-        if grpid > 0
-          data['athletes'].select! { |x| x['cat'].to_i == grpid }
-        else
-          data['athletes']
-        end
+      data  = get_json(comp: params[:wet_id].to_i || 0, type: 'starters')
+      return if data.nil?
+      if grpid > 0
+        data['athletes'].select! { |x| x['cat'].to_i == grpid }
+      else
+        data['athletes']
       end
     end
 
@@ -57,7 +55,7 @@ module Perseus
         route: params[:route].to_i || -1
       }
 
-      data = _get_json(args)
+      data = get_json(args)
       data['participants'] unless data.nil?
     end
   end
@@ -66,7 +64,6 @@ end
 # puts Perseus::EGroupwarePublicAPI.get_starters(wet_id: 5759, grp_id: 81)
 # puts Perseus::EGroupwarePublicAPI.get_results(wet_id: 5759, grp_id: 81, route: 2)
 # puts Perseus::EGroupwarePublicAPI.get_starters(wet_id: 1572,  grp_id: 284)
-#
 #
 # PRIVATE API
 #
