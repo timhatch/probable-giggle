f3fbfc303a100cbb0d2f1550d80ed262e645a8aa
Tim Hatch <tim.hatch@o2.co.uk>
2017-01-02 15:33:51 +0000

    Refactor LocalDBConnection::Startlist

--- ruby/helpers/lanstorage_helper.rb
+++ ruby/helpers/lanstorage_helper.rb
@@ -120,6 +120,22 @@ module Perseus
     module Startlist
       @default_route = { wet_id: 0, grp_id: 0, route: 0 }
 
+      private_class_method
+      
+      # Helper method to create a startlist input
+      # @params
+      # - args, a hash containing :wet_id, :grp_id and :route values (common to all competitors)
+      # - person, a hash containing competitor-unique information such as :per_id, start_order
+      # NOTE: Probably need to add bib_nr here.
+      def self.query args, person
+        person['per_id'] = person['PerId'] unless person['PerId'].nil?
+        args.merge(
+          per_id:         person['per_id'].to_i,
+          start_order:    person['start_order'].to_i,
+          rank_prev_heat: person['rank_prev-heat'].to_i
+        )
+      end
+
       module_function
 
       # Helper method to import a startlist into the LAN database
@@ -127,8 +143,12 @@ module Perseus
       # The data is assumed to ne an array competitors, each a ruby Hash containing at least
       # a per_id (or PerId) and start_order parameter
       #
-      # TODO: Fix the params
-      #
+      # @params
+      # - A has containing:
+      # - :wet_id, :grp_id, :route (each integers) defining the comp, category and round
+      # - :competitors  An array of competitor data, each competitor having three properties
+      #   'PerId' or 'per_id' (the Competitor's id), start_order and rank_prev_heat
+      # REVIEW: Probably we need to add :bib_nr
       def insert params
         args        = Hash[@default_route.map { |k, v| [k, params[k].to_i || v] }]
         competitors = params[:competitors]
@@ -138,13 +158,8 @@ module Perseus
         # snake_case format expected here
         begin
           competitors.each do |person|
-            person['per_id'] = person['PerId'] unless person['PerId'].nil?
-            hash = args.merge(
-              per_id:         person['per_id'].to_i,
-              start_order:    person['start_order'].to_i,
-              rank_prev_heat: person['rank_prev-heat'].to_i
-            )
-            DB[:Results].insert(hash)
+            data = query(args, person)
+            DB[:Results].insert(data)
           end
         rescue
           puts 'nil response'
