b226a3d3a9d163958191ad01dc7dbe3c3c8a5467
Tim Hatch <tim.hatch@o2.co.uk>
2017-02-25 12:26:46 +0000

    Fix CSV parsing
    The CSV parser was returning Hashes with symbolised keys where the later code assumes that keys are strings.
    Add a test to prevent empty rows being added into the output array

--- ruby/helpers/csvparser_helper.rb
+++ ruby/helpers/csvparser_helper.rb
@@ -7,9 +7,11 @@ require 'csv'
 module Perseus
   # CSVParser
   module CSVParser
-    # Parse a csv file containing (as a minimum) per_id and start_order pairs
-    # creating (for each line) a hash corresponding to the data and return an
+    module_function
+
+    # Parse a csv file creating (for each line) a hash corresponding to the data and return an
     # array of these hashes
+    # NOTE: The CSV file MUST CONTAIN HEADERS WITH THE HASH KEY VALUES
     #
     def parse_csv_file params
       array = []
@@ -17,7 +19,8 @@ module Perseus
         file = params[:file][:tempfile]
 
         CSV.foreach(file, headers: true, converters: :numeric) do |row|
-          array << Hash[row.to_hash.map { |k, v| [k.to_sym, v] }]
+          data = Hash[row.to_hash.map { |k, v| [k, v] }]
+          array << data unless data.empty?
         end
       end
       array
