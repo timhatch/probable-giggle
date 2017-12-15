e5f5512a1185d98193589f7c95d4d54fc4aca520
Tim Hatch <hatch.tim@gmail.com>
2017-12-15 12:39:08 +0000

    Fix the lock method
    The prior implementation would lock the results even where locked: false was provided to unlock the results
    i.e. replace
      lock = params[:locked] || true
    by
      lock = params.key?(:locked) ? params[:locked] : true

--- ruby/helpers/localdb_accessors_results.rb
+++ ruby/helpers/localdb_accessors_results.rb
@@ -105,8 +105,8 @@ module Perseus
       # Lock or unlock results (if no :locked parameter is provided the relevant results will be
       # locked automatically
       def results_lock params
-        args = Hash[@default_route.map { |k, v| [k, params[k].to_i || v] }]
-        lock = params[:locked] || true
+        args = query(params)
+        lock = params.key?(:locked) ? params[:locked] : true
         DB[:Results].where(args).update(locked: lock)
       end
     end
