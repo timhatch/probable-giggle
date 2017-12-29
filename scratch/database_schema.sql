4a04f5439961cfb58970c0e1cbe6ee121988c855
Tim Hatch <hatch.tim@gmail.com>
2017-12-29 13:44:16 +0000

    Example to add jsonb column
    Add more commentary

--- scratch/database_schema.sql
+++ scratch/database_schema.sql
@@ -1,33 +1,53 @@
-# Use a view to create a composite of the data needed for external apps
-# instead of using joins in a sequel query
-CREATE VIEW "Ranking" AS
+#
+# Calculate result_rank on querying results
+#
+# Method A - Use a view.
+# + faster
+# - hardwires ranking methodology into the database
+
+CREATE VIEW "BoulderRanking" AS
 SELECT
-  wet_id,grp_id,route,
-  per_id,lastname,firstname,nation,
+  wet_id, grp_id, route, locked,
+  per_id, lastname, firstname, nation, 
   (CAST(sort_values[1] AS TEXT) || 't' || CAST(sort_values[2] AS TEXT) || ' ' || CAST(sort_values[3] AS TEXT) || 'b' || CAST(sort_values[4] AS TEXT)) AS result,
-  result_json,rank_prev_heat,start_order,sort_values,
-  rank() OVER (PARTITION BY wet_id,grp_id,route ORDER BY sort_values[1] DESC NULLS LAST, sort_values[2] ASC, sort_values[3] DESC, sort_values[4] ASC, rank_prev_heat ASC) AS result_rank
-FROM "Results" 
+  result_jsonb, rank_prev_heat, start_order, sort_values,
+  rank() OVER (PARTITION BY wet_id, grp_id, route ORDER BY sort_values[1] DESC NULLS LAST, sort_values[2] ASC, sort_values[3] DESC, sort_values[4] ASC, rank_prev_heat ASC) AS result_rank
+FROM "Results"
 JOIN "Climbers" USING (per_id)
+WHERE locked IS false
 
-# Query whether it would be more efficient to calculate the rank() in a sequel query
-# i.e. the calculation is performed for set parameters
+# Method B - Create the ranking on the fly when querying.
+# + more flexible?
+# - slower
+
+DB[:Results]
+.join(:Climbers, [:per_id])
 .where(params)
-.select{rank.function
-  .over(:order => [
-    Sequel.desc(Sequel.pg_array_op(:sort_values)[1], :nulls=>:last),
-    Sequel.pg_array_op(:sort_values)[2],
-    Sequel.desc(Sequel.pg_array_op(:sort_values)[3]),
-    Sequel.pg_array_op(:sort_values)[3],
-    :rank_prev_heat
-  ])}
+.select(:per_id, :lastname, :firstname, :nation, :start_order, :rank_prev_heat, :sort_values, :result_jsonb)
+.select_append{
+  rank.function.over(
+    partition: [:wet_id, :grp_id: route],
+    order: [
+      Sequel.desc(Sequel.pg_array_op(:sort_values)[1], :nulls=>:last),
+      Sequel.pg_array_op(:sort_values)[2],
+      Sequel.desc(Sequel.pg_array_op(:sort_values)[3]),
+      Sequel.pg_array_op(:sort_values)[3],
+      :rank_prev_heat
+    ]
+  ).as(:result_rank)
+}
 
-
-# Add a column having integer Array type
+# Postgres column types
+#
+# Add a column of type integer Array
 ALTER TABLE "Results" ADD COLUMN "sort_values" INTEGER[4]
-ALTER TABLE "Results" ADD COLUMN "result_json" TEXT DEFAULT '{}'
+# Add a column of type jsonb type
+ALTER TABLE "Results" ADD COLUMN "result_jsonb" jsonb
 
+# Postgres triggers 
+#
 # Triggers in POSTGRESQL use functions, like so...
+#
 CREATE OR REPLACE FUNCTION test_function()
   RETURNS trigger AS
   $BODY$
